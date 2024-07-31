c9c62a15fd0db63c9488b015b1e1e5c231009aa3
Ethan John <ethanjohnlagera@gmail.com>
2024-07-31 23:08:28 +0800

    3rd Commit of the Hour
    Completed rotary encoder code. Removed unnecessary code. Added display() and noDisplay() to everywhere the lcd being used (fixed lcd not working after pressing the button)

--- sketch.ino
+++ sketch.ino
@@ -7,6 +7,7 @@
 #define DHTTYPE DHT22
 #define ENCODER_CLK 13
 #define ENCODER_DT  14
+#define ENCODER_BTN 15
 
 const int pinR = 20;
 const int pinG = 19;
@@ -15,6 +16,7 @@ const int buttonPin = 28;
 int oldButton = LOW;
 int lastClk = HIGH;
 float hotTemp = 32;
+float warmTemp = hotTemp - 2;
 
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
 DHT dht(DHTPIN, DHTTYPE);
@@ -22,21 +24,38 @@ DHT dht(DHTPIN, DHTTYPE);
 void setup() {
   Serial1.begin(115200);
   pinMode(buttonPin, INPUT);
-  pinMode(ENCODER_CLK, INPUT);
-  pinMode(ENCODER_DT, INPUT);
   pinMode(pinR, OUTPUT);
   pinMode(pinG, OUTPUT);
   pinMode(pinB, OUTPUT);
+  pinMode(ENCODER_CLK, INPUT);
+  pinMode(ENCODER_DT, INPUT);
+  pinMode(ENCODER_BTN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_CLK), readEncoder, FALLING);
   
+  lcd.display();
   lcd.begin(16, 2);
   lcd.print("Press the button");
 
   lcd.setCursor(0, 1);
   lcd.print("to check temps");
 
-  delay(5000);
+  delay(4000);
   lcd.clear();
+  lcd.noDisplay();
   dht.begin();
+}
+
+void readEncoder() {
+  int dtValue = digitalRead(ENCODER_DT);
+  if (dtValue == HIGH) {
+    hotTemp++;
+    Serial1.println(hotTemp);
+  }
+  if (dtValue == LOW) {
+    hotTemp--;
+    Serial1.println(hotTemp);
+  }
+  warmTemp = hotTemp - 2;
 }
 
 void loop() {
@@ -49,47 +68,48 @@ void loop() {
 
   // Check if sensor failed and try again.
   if (isnan(humid) || isnan(temp)) {
+    lcd.display();
     lcd.print(F("Failed to read"));
     lcd.setCursor(0, 1);
     lcd.print("from DHT sensor!");
     delay(2000);
     lcd.clear();
+    lcd.noDisplay();
     return;
   }
 
-  int newClk = digitalRead(ENCODER_CLK);
-  if (newClk != lastClk) {
-    // There was a change on the CLK pin
-    lastClk = newClk;
-    int dtValue = digitalRead(ENCODER_DT);
-    if (newClk == LOW && dtValue == HIGH) {
-      hotTemp++;
-      Serial1.println(hotTemp);
-    }
-    if (newClk == LOW && dtValue == LOW) {
-      hotTemp--;
-      Serial1.println(hotTemp);
-    }
-    float warmTemp = hotTemp - 2;
-    float coolTemp = warmTemp - 2;
+  if (digitalRead(ENCODER_BTN) == LOW) {
+    lcd.display();
+    lcd.print("It's hot when");
+    lcd.setCursor(0, 1);
+    lcd.print(String("it's ") + String(hotTemp,0) + (char)223 + String("C"));
+    delay(1000);
+    lcd.clear();
+    lcd.noDisplay();
+    return;
   }
 
   int newButton = digitalRead(buttonPin);
-  if(newButton != oldButton)
-  {
-    if(newButton == HIGH)
-    {
-      lcd.display();
-      lcd.print(String("It's ") + String(temp,0) + (char)223 + String("C ") + String("H: ") + String(humid,0) + String("%"));
-      lcd.setCursor(0, 1);
-      lcd.print(String("Feels Like: " + String(feelsLike,0) + (char)223 + String("C)")));
-      delay(5000);
-      lcd.clear();
-      lcd.noDisplay();
+  if(newButton == HIGH) {
+    if (temp >= hotTemp){
+      analogWrite(pinR, 255);
+    } else if (temp >= warmTemp){
+      analogWrite(pinR, 255);
+      analogWrite(pinG, 255);
+    } else if (temp < warmTemp){
+      analogWrite(pinB, 255);
     }
-    oldButton = newButton;
+    lcd.display();
+    lcd.print(String("It's ") + String(temp,0) + (char)223 + String("C ") + String("H: ") + String(humid,0) + String("%"));
+    lcd.setCursor(0, 1);
+    lcd.print(String("Feels Like: " + String(feelsLike,0) + (char)223 + String("C)")));
+    delay(5000);
+    analogWrite(pinR, 0);
+    analogWrite(pinG, 0);
+    analogWrite(pinB, 0);
+    lcd.clear();
+    lcd.noDisplay();
   }
 
   delay(100); // this speeds up the simulation
-
 }
